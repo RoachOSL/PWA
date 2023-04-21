c8b7965fbed3df5dd93f7e43d9d315f0d0e49ec8
Roach <os1996@o2.pl>
2023-04-21 12:10:26 +0200

    Dodanie manifestu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 function vibrate() {
     navigator.vibrate([500, 500, 500, 1000, 1000, 1000, 500, 500, 500])
 }
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+}
---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+const staticHealAnima = "dev-healthy-animal-site-v1"
+const assets = [
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticHealAnima).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+})
